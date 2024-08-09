ad67218743bd4a88d9944828feecba0721cab52d
wycj2009 <wycj2009@naver.com>
2024-08-09 17:58:56 +0900

    Compose Navigation 구현

--- compose_navigation/build.gradle.kts
+++ compose_navigation/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
