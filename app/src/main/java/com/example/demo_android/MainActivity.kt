505cb6bc6cb234f592b199dd50bb84eaacea57cd
wycj2009 <wycj2009@naver.com>
2024-08-09 15:57:28 +0900

    코드 정리

--- app/src/main/java/com/example/demo_android/MainActivity.kt
+++ app/src/main/java/com/example/demo_android/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Demo_AndroidTheme {
                 Box(modifier = Modifier.fillMaxSize()) {
--- compose_navigation/src/main/java/com/example/compose_navigation/MainActivity.kt
+++ compose_navigation/src/main/java/com/example/compose_navigation/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Demo_AndroidTheme {
                 Text(text = "compose_navigation")
