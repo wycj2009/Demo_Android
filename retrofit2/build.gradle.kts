5cf434a30b1bdc172ea8ddc84126812d068ee66f
wycj2009 <wycj2009@naver.com>
2024-10-16 02:28:36 +0900

    retrofit2 모듈 추가

---
+++ retrofit2/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.demo_android.retrofit2"
+    compileSdk = BuildInfo.COMPILE_SDK
+
+    defaultConfig {
+        applicationId = BuildInfo.APPLICATION_ID
+        minSdk = BuildInfo.MIN_SDK
+        targetSdk = BuildInfo.TARGET_SDK
+        versionCode = BuildInfo.VERSION_CODE
+        versionName = BuildInfo.VERSION_NAME
+    }
+    compileOptions {
+        sourceCompatibility = BuildInfo.JAVA_VERSION
+        targetCompatibility = BuildInfo.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = BuildInfo.JVM_TARGET
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = BuildInfo.COMPOSE_KOTLIN_COMPILER_EXTENSION_VERSION
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
