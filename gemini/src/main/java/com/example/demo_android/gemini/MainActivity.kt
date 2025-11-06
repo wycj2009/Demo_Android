e3f1fa93d6ac0053751cd2c905bedb12fdafa8e7
wycj2009 <wycj2009@naver.com>
2025-11-06 17:30:12 +0900

    gemini 모듈 추가

---
+++ gemini/src/main/java/com/example/demo_android/gemini/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.demo_android.gemini
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+        }
+    }
+}
