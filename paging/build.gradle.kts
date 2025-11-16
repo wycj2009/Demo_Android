6c61396fadc484b562974ba42bd83c23e13504d8
wycj2009 <wycj2009@naver.com>
2025-11-17 08:53:04 +0900

    paging 모듈 추가

---
+++ paging/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.compose)
+}
+
+android {
+    namespace = "${BuildInfo.APPLICATION_ID}.paging"
+    compileSdk = BuildInfo.COMPILE_SDK
+
+    defaultConfig {
+        applicationId = BuildInfo.APPLICATION_ID
+        minSdk = BuildInfo.MIN_SDK
+        targetSdk = BuildInfo.TARGET_SDK
+        versionCode = BuildInfo.VERSION_CODE
+        versionName = BuildInfo.VERSION_NAME
+    }
+    compileOptions {
+        sourceCompatibility = BuildInfo.JAVA_VERSION
+        targetCompatibility = BuildInfo.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = BuildInfo.JVM_TARGET
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = BuildInfo.COMPOSE_KOTLIN_COMPILER_EXTENSION_VERSION
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
---
+++ paging/src/main/java/com/example/demo_android/paging/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.demo_android.paging
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
